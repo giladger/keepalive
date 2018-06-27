32dfd6ae5fcf9b1ad476f33a57d14760696521ac
Gilad Gershtein <Gilad.Gershtein@emc.com>
2018-06-27 13:22:41 +0300

    added redis

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask
 from datetime import datetime
+import os
+import redis
+
+r = redis.from_url(os.environ.get("REDIS_URL"))
+
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    now = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    previous = r.get('time')
+    r.set('time', now)
 
-    return """
+    return f"""
     <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+    <p>It is currently {now}.</p>
+    <p>Previous time was: {previous}.</p>
+    """
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
