a9c39325a5f3373b52df62da1f202c5c417e0664
Gilad Gershtein <Gilad.Gershtein@emc.com>
2018-06-27 19:15:46 +0300

    added scheduling

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
 from flask import Flask
-from datetime import datetime
+import time
 import os
-#import redis
+import redis
 from telegram import Bot
 
-#r = redis.from_url(os.environ.get("REDIS_URL"))
+r = redis.from_url(os.environ.get("REDIS_URL"))
 telegram = Bot(os.environ.get("TELEGRAM_BOT_TOKEN"))
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    now = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-    previous = os.environ.get('LAST_ACCESS_TIME')
-    os.environ['LAST_ACCESS_TIME'] = now
+    now = time.time()
+    previous = r.get('connection_time')
+    r.set('connection_time', now)
     telegram.send_message(os.environ.get("TELEGRAM_USER_ID"), f"Hello! {now}")
 
-    return f"""
-    <h1>Hello heroku</h1>
-    <p>It is currently {now}.</p>
-    <p>Previous time was: {previous}.</p>
-    """
+    return ""
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
--- clock.py
+++ clock.py
@@ -1,13 +1,18 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import time
+import redis
+from telegram import Bot
+
+r = redis.from_url(os.environ.get("REDIS_URL"))
+telegram = Bot(os.environ.get("TELEGRAM_BOT_TOKEN"))
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
-    print('This job is run every three minutes.')
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    now = time.time()
+    last_connection = r.get('connection_time')
+    if last_connection and (now - last_connection) > 60:
+        telegram.send_message(os.environ.get("TELEGRAM_USER_ID"), 'No connection')
 
 sched.start()
