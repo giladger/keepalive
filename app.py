ed53dead860ed1b4a006f26326f049288aaadfe3
Gilad Gershtein <Gilad.Gershtein@emc.com>
2018-06-27 18:29:00 +0300

    added telegram

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask
 from datetime import datetime
 import os
 import redis
+from telegram import Bot
 
 r = redis.from_url(os.environ.get("REDIS_URL"))
+telegram = Bot(os.environ.get("TELEGRAM_BOT_TOKEN"))
 
 app = Flask(__name__)
 
@@ -12,6 +14,7 @@ def homepage():
     now = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
     previous = r.get('time')
     r.set('time', now)
+    telegram.send_message(os.environ.get("TELEGRAM_UESR_ID"), f"Hello! {now}")
 
     return f"""
     <h1>Hello heroku</h1>
